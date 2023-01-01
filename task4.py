b2f667e0231ca11f74005afdd1ec1a35696005c4
kshaternik <kseniyashaternik@gmail.com>
2023-01-01 20:23:53 +0300

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,12 @@
+#4. Задайте список из произвольных вещественных чисел, количество задаёт пользователь.
+#Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+list = [5.16, 8.62, 6.57, 7.92, 9.22]
+print(list)
+
+def dif(list):
+    dif_max_min =[]
+    for i in range(len(list)):
+        dif_max_min.append(list[i]%1)
+    return max(dif_max_min) - min(dif_max_min)
+print(round(dif(list),2))
